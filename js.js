1f08c72c2459474140113a2cfa5c23864d6e0102
Abujiab <abujiabcs@gmail.com>
2025-09-27 00:21:13 +0300

    js file

---
+++ js.js
@@ -0,0 +1,58 @@
+// let num=[1,2,3,4,5];
+// console.log(num);
+
+// let num=[1,2,3,4,5];
+// console.log(Array.isArray(num));
+
+// let old = [0, "a", 1, "b", 2, "c"];
+// let num = Array.from(old).filter(x => isNaN(x));
+// console.log(num);
+
+// let num =[1,2,3,4,5];
+// num.push(6);
+// console.log(num);
+
+
+// let num =[1,2,3];
+// let sec=[4,5,6];
+
+// let nums=num.concat(sec);
+// console.log(nums);
+
+
+// let num = [1, 2, 3, 4, 5, 6];
+// let part1 = num.slice(0, 2);
+// let part2 = num.slice(4);
+// let result = part1.concat(part2);
+// console.log(result);
+
+// let num = [1, 2, 3, 4, 5];
+// console.log(num.join());
+
+// let num = [1, 2, 3];
+// console.log(Array.of(num));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
